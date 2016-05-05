c60631917d99671d3b41c90cde065bfa2f282881
Sebaaaa <sebastian.gaete.14@sansano.usm.cl>
2016-05-05 04:15:01 -0300

    cambio nuevamente para ver push

--- Hola/src/Principal.java
+++ Hola/src/Principal.java
@@ -2,7 +2,7 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Git!! estoy probando desde atom ahora!!.....lo edito nuevamente para ver push");
+		System.out.println("Hola Git!! estoy probando desde atom ahora!!...lo edito nuevamente para ver push");
 
 		
 
